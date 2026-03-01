ad303f421dae484492af6932a181b0b215d4bf20
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-03-01 20:20:47 +0100

    feat: add lifecycle methods and setInterval to RandomChar

--- src/components/random-char/RandomChar.jsx
+++ src/components/random-char/RandomChar.jsx
@@ -1,39 +1,64 @@
 import { Component } from "react";
 import "./randomChar.scss";
 
-import thor from "../../resources/img/thor.jpg";
 import mjolnir from "../../resources/img/mjolnir.png";
+import marverService from "../../services/service";
+import Loader from "../loader/loader";
 
 export default class RandomChar extends Component {
+
+    state = {
+        char: {},
+        isLoading: true,
+        isError: false
+    };
+
+    
+    componentDidMount() {
+        this.updateChar();
+        this.timerId = setInterval(this.updateChar, 3000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+
+    marverService = new marverService();
+    
+    onCharLoaded = (char) => {
+        this.setState({ char, isLoading: false, isError: false});
+    }
+
+    onError = () => {
+        this.setState({ isLoading: false, isError: true});
+    }
+
+    updateChar = () => {
+        this.setState({ isLoading: true, isError: false});
+        this.marverService.getCharacter(Math.floor(Math.random() * (21 - 1) + 1))
+        .then(this.onCharLoaded)
+        .catch(this.onError);
+    }
+
     render() {
+        const {char, isLoading, isError} = this.state;
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thor} alt="Thor" className="randomchar__img" />
-                    <div className="randomchar_info">
-                        <div className="randomchar__name">Thor</div>
-                        <p className="randomchar__descr">
-                            As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
-                        </p>
-                        <div className="randomchar_btns">
-                            <a href="#" className="btn btn__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href="#" className="btn btn__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {isError ? <p style={{textAlign: "center"}}>Error</p> : null}
+                {isLoading ? <Loader /> : null}
+                {!(isLoading || isError) ? <View char={char} /> : null}
+
                 <div className="randomchar__static">
-                    <p className="randomchar_title">
+                    <p className="randomchar__title">
                          Random character for today!<br />
                          Do you want to get to know him better?
                     </p>
                     <p className="randomchar__title">
                         Or choose another one!
                     </p>
-                    <button className="btn btn__main">
+                    <button 
+                        className="btn btn__main"
+                        onClick={this.updateChar}>
                         <div className="inner">Try It</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -43,3 +68,33 @@ export default class RandomChar extends Component {
     }
 }
 
+const View = ({char}) => {
+    const { name, description, thumbnail, homepage, wiki } = char;
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt={name} className="randomchar__img" />
+            <div className="randomchar__info">
+                <div className="randomchar__name">{name}</div>
+                <p className="randomchar__description">{description}</p>
+                <div className="randomchar__btns">
+                    <a 
+                        href={homepage}
+                        className="btn btn__main"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a 
+                    href={wiki} 
+                    className="btn btn__main"
+                    rel="noreferrer"
+                    target="_blank"
+                    >
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
