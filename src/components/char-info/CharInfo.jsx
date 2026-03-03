519f2a985e52e82e2233a7a293e5ee39ae0bb582
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-03-04 00:58:38 +0100

    fixed import path

--- src/components/char-info/CharInfo.jsx
+++ src/components/char-info/CharInfo.jsx
@@ -1,60 +1,111 @@
-import React, { Component } from 'react'
-import thor from "../../resources/img/thor.jpg"
-
+import React, { Component, Fragment } from 'react'
+import marverService from "../../services/service";
+import Loader from '../loader/loader';
+import Error from '../error/Error';
+import Skeleton from '../skeleton/Skeleton';
 import "./charInfo.scss";
 
 export default class CharInfo extends Component {
+  state = {
+    char: {},
+    isLoading: true,
+    isRequestLoading: false,
+    isError: false
+  }
+
+  marverService = new marverService();
+
+  componentDidMount() {
+    this.updateChar();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.charId !== prevProps.charId) {
+      this.updateChar();
+    }
+  }
+
+  
+
+  componentDidCatch(err, info) {
+    console.log(err, info);
+    this.setState({isLoading: false, isError: true});
+  }
+
+  updateChar = () => {
+    const { charId } = this.props;
+    if (!charId) {
+      return;
+    }
+
+    this.setState({ isLoading: true, isError: false });
+    this.marverService.getCharacter(charId)
+      .then(this.onCharLoaded)
+      .catch(this.onError);
+  }
+
+  onCharLoaded = (char) => {
+    this.setState({ char, isLoading: false, isError: false });
+  }
+
+  onError = () => {
+    this.setState({ isLoading: false, isError: true });
+  }
+
+
   render() {
+    const { char, isLoading, isError } = this.state;
     return (
       <div className="char__info">
-        <div className="char__basics">
-          <img src={thor} alt="thor" />
-          <div>
-            <div className="char__info-name">thor</div>
-            <div className="char__btns">
-              <a href="#" className="btn btn__main">
-                <div className="inner">homepage</div>
-              </a>
-              <a href="#" className="btn btn__secondary">
-                <div className="inner">Wiki</div>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="char__descr">
-          In Norse mythology, Loki is a god or jötunn (or both). Loki is the son of Fárbauti and Laufey, and the brother of Helblindi and Býleistr. By the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir, and the world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or Narfi and with the stallion Svaðilfari as the father, Loki gave birth—in the form of a mare—to the eight-legged horse Sleipnir. In addition, Loki is referred to as the father of Váli in the Prose Edda.
-        </div>
-        <div className="char__comics">Comics:</div>
-        <ul className="char__comics-list">
-          <li className="char__comics-item">
-            All-Winners Squad: Band of Heroes (2011) #3
-          </li>
-          <li className="char__comics-item">
-            Amazing Spider-Man (1999) #503
-          </li>
-          <li className="char__comics-item">
-            Amazing Spider-Man (1999) #504
-          </li>
-          <li className="char__comics-item">
-            AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-          </li>
-          <li className="char__comics-item">
-            Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-          </li>
-          <li className="char__comics-item">
-            Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-          </li>
-          <li className="char__comics-item">
-            Vengeance (2011) #4
-          </li>
-          <li className="char__comics-item">
-            Avengers (1963) #1
-          </li>
-          <li className="char__comics-item">
-            Avengers (1996) #1
-          </li>
-        </ul>
+        {char || isLoading || isError ? null : <Skeleton />}
+        {isError ? <Error /> : null}
+        {isLoading ? <Loader /> : null}
+        {!(isLoading || isError || !char) ? <View char={char} /> : null}
       </div>
     );
   }
+}
+
+const View = ({ char }) => {
+  return (
+    <Fragment>
+      <div className="char__basics">
+        <img src={char.thumbnail} alt={char.name} />
+        <div>
+          <div className="char__info-name">{char.name}</div>
+          <div className="char__btns">
+            <a
+              href={char.homepage}
+              className="btn btn__main"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <div className="inner">homepage</div>
+            </a>
+            <a
+              href={char.wiki}
+              className="btn btn__secondary"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <div className="inner">Wiki</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="char__descr">
+        {char.description}
+      </div>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">
+        {char.comics.map((comic => {
+          return (
+            <li className="char__comics-item" key={comic.name}>
+              {comic}
+            </li>
+          )
+        }))}
+      </ul>
+    </Fragment>
+  )
 }
