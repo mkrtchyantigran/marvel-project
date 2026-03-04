5431cc4b1efb91221f73c2e2f13bd79e20f6b323
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-03-04 01:08:42 +0100

    added states isRequestLoading, isNoMoreChars, offset and functiones onRequest, onCharListLoading, and used componentDidUpdate for updating our state and rerender with new + 6 charList

--- src/components/char-list/CharList.jsx
+++ src/components/char-list/CharList.jsx
@@ -1,49 +1,96 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
+import marvelService from '../../services/service';
 import "./charList.scss";
-import abyss from "../../resources/img/abyss.jpg";
-
+import Loader from '../loader/loader';
+import Error from '../error/Error';
 export default class CharList extends Component {
+
+  state = {
+    charList: [],
+    isLoading: true,
+    isRequestLoading: false,
+    isError: false,
+    isNoMoreChars: false,
+    offset: 0
+  }
+
+  marvelService = new marvelService();
+
+  componentDidMount() {
+    this.onRequest();
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.offset !== this.state.offset) {
+      this.setState({
+        isNoMoreChars: this.state.offset > this.state.charList.length
+      })
+    }
+  }
+
+  onRequest(offset = 0) {
+    this.marvelService.getCharactersAll(offset)
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  }
+
+  onCharListLoading = () => {
+    this.setState({ isRequestLoading: true });
+  }
+
+
+  onCharListLoaded = (newCharList) => {
+    this.setState(({ offset, charList }) => {
+      return {
+        charList: [...charList, ...newCharList],
+        isLoading: false,
+        isError: false,
+        offset: offset + 6
+      }
+    });
+  }
+
+  onError = () => {
+    this.setState({ isError: true, isLoading: false });
+  }
+
+  renderItems(arr) {
+    const items = arr.map((item) => {
+      return (
+        <li
+          onClick={() => this.props.onCharSelected(item.id)}
+          className="char__item"
+          key={item.id}
+        >
+          <img src={item.thumbnail ? item.thumbnail : "Error"} alt={item.name} />
+          <div className="char__name">{item.name}</div>
+        </li>
+      )
+    })
+    return <ul className="char__grid">{items}</ul>;
+  }
+
   render() {
+    const { charList, isRequestLoading, isLoading, isError, isNoMoreChars } = this.state;
+
+    const items = this.renderItems(charList);
+
     return (
       <div className="char__list">
-        <ul className="char__grid">
-          <li className="char__item">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-          <li className="char__item char__item_selected">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-          <li className="char__item">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-          <li className="char__item">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-          <li className="char__item">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-          <li className="char__item">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-          <li className="char__item">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-          <li className="char__item">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-          <li className="char__item">
-            <img src={abyss} alt="abyss"/>
-            <div className="char__name">Abyss</div>
-          </li>
-        </ul>
+
+        {isError ? <Error /> : null}
+        {isLoading || isRequestLoading ? <Loader /> : null}
+        {!(isLoading || isRequestLoading || isError) ? items : null}
+
+        {
+          isNoMoreChars ? null : <button
+              onClick={() => this.onRequest(this.state.offset)}
+              className='btn btn__main btn__long'
+              disabled={this.state.isRequestLoading}
+            >
+              <div className="inner">{this.state.isRequestLoading ? "loading..." : "load more"}</div>
+            </button>
+        }
       </div>
     );
   }
